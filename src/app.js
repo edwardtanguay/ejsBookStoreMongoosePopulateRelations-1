fc0d4d796334ce65548e699719382cc02e425654
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-25 22:03:55 +0100

    update env

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import * as BooksController from "./controllers/books.js";
+import dotenv from "dotenv";
 
-mongoose.connect("mongodb://localhost:27017/bookstore");
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_CONNECT);
 
 const app = express();
 const __dirname = path.resolve(path.dirname(""));
@@ -12,11 +15,11 @@ const staticDirectory = path.join(__dirname, "./public");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./src/views"));
+
 app.use(express.static(staticDirectory));
 
 app.get("/", async (req, res) => {
   const books = await BooksController.getAllBooks();
-  console.log(books[0].comments);
   res.render("index", {
     pageTitle: "Tech Bookstore",
     books
