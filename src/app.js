1755b05871da0df76fdeec189a90b3f61b47968d
pierluigi <pierluigibaiano@gmail.com>
2021-11-25 10:57:47 +0100

    import BooksController, update endroute and test the books in index.ejs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import path from "path";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import * as BooksController from "./controllers/books.js";
 
-mongoose.connect('mongodb://localhost:27017/bookstore');
+mongoose.connect("mongodb://localhost:27017/bookstore");
 
 const app = express();
 const __dirname = path.resolve(path.dirname(""));
@@ -13,13 +14,13 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./src/views"));
 app.use(express.static(staticDirectory));
 
-app.get('/', async (req, res) => {
-    // res.send('testing');
-    res.render('index',{
-        pageTitle: "Tech Bookstore"
-    });
+app.get("/", async (req, res) => {
+  const books = await BooksController.getAllBooks();
+  res.render("index", {
+    pageTitle: "Tech Bookstore",
+    books, // alternative books:books
+  });
 });
-
 
 app.listen(port, () => {
   console.log(`Now listening on port http://localhost:${port}`);
